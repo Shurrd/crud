cf0c6d04bc165b3b5b50507a8fac83bc0b1cce61
shurrd <engraaadeyemi@gmail.com>
2024-08-22 12:03:16 +0100

    feat: get transaction details by logged inuser

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -11,26 +11,39 @@ import {
 import { CreateTransactionDto } from './dtos';
 import { RequestWithUser } from 'src/types';
 import { TransactionsService } from './transactions.service';
-import { AuthGuard } from 'src/common/guards';
+import { AuthGuard, RoleGuard } from 'src/common/guards';
+import { Roles } from 'src/common/decorators';
+import { Role } from 'src/common/enums';
 
 @UseGuards(AuthGuard)
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
+  @Roles(Role.ADMIN, Role.MANAGER)
+  @UseGuards(RoleGuard)
   @Get()
   async getAllTransactions() {
     return await this.transactionsService.getAllTransactions();
   }
 
+  @Roles(Role.ADMIN, Role.MANAGER)
+  @UseGuards(RoleGuard)
   @Get('user')
   async getTransactionsByUser(@Query('userId') userId: number) {
     return await this.transactionsService.getTransactionsByUser(userId);
   }
 
+  @Roles(Role.ADMIN, Role.MANAGER)
+  @UseGuards(RoleGuard)
   @Get(':id')
   async getTransactionById(@Param('id') transactionId: string) {
     return await this.transactionsService.getTransactionById(transactionId);
+  }
+
+  @Get('account/details')
+  async getTransactionsByLoggedInUser(@Req() req: RequestWithUser) {
+    return await this.transactionsService.getTransactionsByUser(req.user.id);
   }
 
   @Post()
